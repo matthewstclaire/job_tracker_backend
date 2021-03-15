cab550bdefb884db8c04a6bd920678a1090dd767
matthewstclaire <matthew.crnich@gmail.com>
2021-03-15 10:16:14 -0600

    First commit

---
+++ db/migrate/20210314181023_create_jobs.rb
@@ -0,0 +1,15 @@
+class CreateJobs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :date_applied
+      t.string :company
+      t.string :next_steps
+      t.integer :interest
+      t.boolean :open
+      t.string :applied_on
+
+      t.timestamps
+    end
+  end
+end
